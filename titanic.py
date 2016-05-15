85d5be98c5051b20b957966a78be5c2343148bdf
Michael D. Misamore <m.misamore@gmail.com>
2016-05-15 17:47:10 -0400

    Some more feature analysis

--- titanic.py
+++ titanic.py
@@ -17,11 +17,11 @@ train.replace({'Sex': sexMap}, inplace=True)
 test.replace({'Sex': sexMap}, inplace=True)
 
 # Embarked feature
-embarkedMap = {'S': 1, 'C': 1, 'Q': 2}
+embarkedMap = {'S': 1, 'C': 2, 'Q': 2}
 train.replace({'Embarked': embarkedMap}, inplace=True)
-train['Embarked'].fillna(1.0, inplace=True)
+train['Embarked'].fillna(0, inplace=True)
 test.replace({'Embarked': embarkedMap}, inplace=True)
-test['Embarked'].fillna(1.0, inplace=True)
+test['Embarked'].fillna(0, inplace=True)
 
 # Match last part of ticket number
 ticketNum = r"(\d+)$"
@@ -39,6 +39,7 @@ train['Ticket'] = pd.to_numeric([regexHelper(ticketNum,ticket) for ticket in
 test['Ticket']  = pd.to_numeric([regexHelper(ticketNum,ticket) for ticket in
     test['Ticket']])
 
+# Impute gaps
 trainMin = pd.Series.min(train['Ticket'])
 trainMax = pd.Series.max(train['Ticket'])
 train['Ticket'].fillna((trainMax-trainMin)/2.0+trainMin,inplace=True)
@@ -50,8 +51,13 @@ test['Ticket'].fillna((testMax-testMin)/2.0+testMin,inplace=True)
 cabinMap = {'n': 1, 'A': 2, 'B': 3, 'C': 4, 'D': 5, 'E': 6, 'F': 7, 'G': 8, 'T': 9 }
 train['Cabin'] = [str(c)[0] for c in train['Cabin']]
 train.replace({'Cabin': cabinMap}, inplace=True)
+
 test['Cabin']  = [str(c)[0] for c in test['Cabin']]
 test.replace({'Cabin': cabinMap}, inplace=True)
+
+# Simplify to "had cabin or not"
+train['Cabin'] = train['Cabin'] == 1
+test['Cabin']  = test['Cabin'] == 1
 
 # Fix the Age feature to remove NaN
 train['Age'].fillna(28.0, inplace=True)
@@ -80,16 +86,9 @@ for attr in xTest.columns.values:
 xTrain['PassengerId'] = train['PassengerId']
 xTest['PassengerId']  = test['PassengerId']
 
-# Begin analysis
-# fullSet = xTrain.copy()
-# fullSet['Survived'] = yTrain
-
-# Strong correlations with Survived: Sex, Pclass, Cabin, Fare, Embarked
-# print fullSet.corr()
-
 # Try PCA in the pipeline
 # from sklearn.decomposition import PCA
-# pca = PCA(n_components=10)
+# pca = PCA(n_components=4,whiten=True)
 # xTrain = pd.DataFrame(pca.fit_transform(xTrain.drop('PassengerId',axis=1)))
 # xTrain['PassengerId'] = train['PassengerId']
 
@@ -103,21 +102,21 @@ from sklearn.grid_search import GridSearchCV
 #    'weights': ['uniform','distance']
 # })
  
-from sklearn.tree import DecisionTreeClassifier
-c = DecisionTreeClassifier() # Accuracy = 0.82 
-clf = GridSearchCV(c, {
-    'min_samples_split': range(1,20), 
-    'max_features': range(1,len(xTrain.columns.values)-1),
-    'random_state': [1,10,12,23,42]
-})
-
-# from sklearn.ensemble import RandomForestClassifier
-# c = RandomForestClassifier() # Accuracy = 0.81
+# from sklearn.tree import DecisionTreeClassifier
+# c = DecisionTreeClassifier() # Accuracy = 0.84 
 # clf = GridSearchCV(c, {
-#    'min_samples_split': range(1,20), 
-#     'n_estimators': range(5,15),
-#     'random_state': [42]
+#     'min_samples_split': range(1,20), 
+#     'max_features': range(1,len(xTrain.columns.values)-1),
+#     'random_state': [1,10,12,23,42]
 # })
+ 
+from sklearn.ensemble import RandomForestClassifier
+c = RandomForestClassifier() # Accuracy = 0.83
+clf = GridSearchCV(c, {
+   'min_samples_split': range(1,20), 
+    'n_estimators': range(5,15),
+    'random_state': [42]
+})
 
 # from sklearn.ensemble import AdaBoostClassifier 
 # c = AdaBoostClassifier() # Accuracy = 0.79 
@@ -147,6 +146,7 @@ clf = GridSearchCV(c, {
 #    'random_state': [1,10,12,23,42]
 # })
 
+'''
 # 5-fold cross-validation score
 from sklearn.cross_validation import cross_val_score
 print "Cross val score: ", cross_val_score(
@@ -164,17 +164,18 @@ featuresTrain, featuresTest, labelsTrain, labelsTest = train_test_split(
     train_size=0.80, 
     random_state=42
 )
+'''
 
 # Fit and predict
-clf.fit(featuresTrain,labelsTrain)
-pred = clf.predict(featuresTest)
+# clf.fit(featuresTrain,labelsTrain)
+# pred = clf.predict(featuresTest)
 
 # SUBMISSIONS
-# clf.fit(xTrain.drop('PassengerId',axis=1),yTrain)
-# pred = clf.predict(xTest.drop('PassengerId',axis=1))
-# Create and write the submission to dis
-# submission = pd.DataFrame({'Survived': pred}, index=xTest['PassengerId'])
-# submission.to_csv("submission.csv")
+clf.fit(xTrain.drop('PassengerId',axis=1),yTrain)
+pred = clf.predict(xTest.drop('PassengerId',axis=1))
+# Create and write the submission to disk
+submission = pd.DataFrame({'Survived': pred}, index=xTest['PassengerId'])
+submission.to_csv("submission.csv")
 
 # Scoring for training before submission:
 
@@ -182,6 +183,7 @@ pred = clf.predict(featuresTest)
 # print "Feature importances: ", zip(featuresTrain.columns.values,
 #        clf.feature_importances_)
 
+'''
 # Scoring via accuracy
 from sklearn.metrics import accuracy_score
 print "Accuracy: ", accuracy_score(labelsTest,pred)
@@ -196,4 +198,4 @@ precision, recall, fscore, _ = precision_recall_fscore_support(
 print "Precision: ", precision
 print "Recall: ", recall
 print "F-Score: ", fscore
-
+'''
