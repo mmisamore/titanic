e6c314b482a55d2e72a3b584df33f4d553c9d76e
Michael D. Misamore <m.misamore@gmail.com>
2016-05-21 11:00:01 -0400

    Add 'title' feature and play with tuning hyperparameters on RF

--- titanic.py
+++ titanic.py
@@ -8,6 +8,38 @@ train = pd.read_csv('train.csv')
 test  = pd.read_csv('test.csv')
 
 ### Begin Feature Engineering
+
+# Code titles from Names
+def title(name):
+    if 'Mr.' in name:
+        return 0
+    elif 'Mrs.' in name or 'Mme.' in name:
+        return 1
+    elif 'Miss.' in name or 'Ms.' in name or 'Mlle.' in name:
+        return 2
+    elif 'Master.' in name:
+        return 3
+    elif 'Dr.' in name:
+        return 4
+    elif 'Countess' in name:
+        return 5
+    elif 'Rev.' in name:
+        return 6
+    elif 'Major.' in name or 'Col.' in name:
+        return 7
+    elif 'Sir.' in name or 'Lady.' in name or 'Don.' in name:
+        return 8
+    elif 'Capt.' in name:
+        return 9
+    else:
+        return 10 
+
+titlesTrain = pd.DataFrame({'titles': [title(n) for n in train['Name']]})
+train['titles'] = titlesTrain['titles']
+
+titlesTest = pd.DataFrame({'titles': [title(n) for n in test['Name']]})
+test['titles'] = titlesTest['titles']
+
 train.drop('Name', axis=1, inplace=True)
 test.drop('Name', axis=1, inplace=True)
 
@@ -85,89 +117,79 @@ for attr in xTest.columns.values:
 xTrain['PassengerId'] = train['PassengerId']
 xTest['PassengerId']  = test['PassengerId']
 
-# Try PCA in the pipeline
-# from sklearn.decomposition import PCA
-# pca = PCA(n_components=5,whiten=True)
-# xTrain = pd.DataFrame(pca.fit_transform(xTrain.drop('PassengerId',axis=1)))
-# xTrain['PassengerId'] = train['PassengerId']
-
 # Deploy a classifier
-# from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.grid_search import GridSearchCV
+from sklearn.feature_selection import SelectKBest
 
 # from sklearn.neighbors import KNeighborsClassifier
-# clf = KNeighborsClassifier(n_neighbors=10) # Accuracy = 0.81 
- 
-# from sklearn.tree import DecisionTreeClassifier
-# clf = DecisionTreeClassifier() # Accuracy = 0.78
- 
-# from sklearn.ensemble import RandomForestClassifier
-# clf = RandomForestClassifier(n_estimators=1000,criterion='entropy') # 0.79
+# clf = KNeighborsClassifier()
+# params = {
+#     'p':         [1,2],
+#     'leaf_size': [1,2,4,8,16,32,64],
+#     'algorithm': ['ball_tree','kd_tree','brute'],
+#     'weights':   ['uniform','distance'],
+#     'n_neighbors': [1,2,4,6,8,10,15,20]
+# }
+# gs = GridSearchCV(clf, params, cv=5, scoring='accuracy')
+
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier()
+params = {
+    'n_estimators': [50],
+    'max_features': [1],
+    'max_depth':    [10],
+    'random_state': [1,2,4,8,16,32],
+    'n_jobs':       [-1]
+}
+gs = GridSearchCV(clf, params, cv=5, scoring='accuracy') 
 
 # Split into artificial train and test sets
-# from sklearn.cross_validation import train_test_split
-# featuresTrain, featuresTest, labelsTrain, labelsTest = train_test_split(
-#     xTrain.drop('PassengerId',axis=1), 
-#     yTrain, 
-#     train_size=0.20 
-# )
+from sklearn.cross_validation import train_test_split
+featuresTrain, featuresTest, labelsTrain, labelsTest = train_test_split(
+    xTrain.drop('PassengerId',axis=1), 
+    yTrain, 
+    train_size=0.80,
+    random_state=42
+)
 
 # Fit and predict
-# clf.fit(xTrain.drop('PassengerId',axis=1),yTrain)
-# print zip(xTrain.columns.values, clf.feature_importances_)
-# exit(0)
-
-# clf.fit(featuresTrain,labelsTrain)
-
-xTrain['Survived'] = train['Survived']
-# print xTrain.corr() - Tells us that females is the first split, more likely to survive
-# xTrain = xTrain[xTrain['Sex'] == 1].drop('Sex',axis=1)
-# Class 0 more likely to have survived than not for Females
-# print len(xTrain[(xTrain['Pclass'] == 0) & (xTrain['Survived'] == 0)])
-# Class 0.5 mostly survived as well for females
-# print len(xTrain[(xTrain['Pclass'] == 0.5) & (xTrain['Survived'] == 0)])
-# Class 1 is a completely even split for females: just as likely to survive or perish
-# print len(xTrain[(xTrain['Pclass'] == 1) & (xTrain['Survived'] == 0)])
-# xTrain = xTrain[xTrain['Pclass'] == 1].drop('Pclass',axis=1)
-# If Embarked = 0, more likely to perish than survive
-# print len(xTrain[(xTrain['Embarked'] == 0) & (xTrain['Survived'] == 0)])
-# If Embarked = 0.5, more likely to survive than perish
-# print len(xTrain[(xTrain['Embarked'] == 0.5) & (xTrain['Survived'] == 1)])
-# If Embarked = 1, more likely to survive than perish
-# print len(xTrain[(xTrain['Embarked'] == 1) & (xTrain['Survived'] == 1)])
-
-# Suggests that cabin = 1 for Men correlated with lower survival rate
-# xTrain = xTrain[xTrain['Sex'] == 0].drop('Sex',axis=1)
-# print xTrain.corr()
-
-def predictManual(record):
-    if record.Sex == 0:
-        return 0            
-    else: # Females
-        if record.Pclass == 0:
-            return 1
-        elif record.Pclass == 0.5:
-            return 1
-        else:
-            if record.Embarked == 0:
-                return 0
-            elif record.Embarked == 0.5:
-                return 1
-            else:
-                return 1
+gs.fit(featuresTrain,labelsTrain)
+pred = gs.predict(featuresTest)
+ 
+# Manually built decision tree with a decent score. Tuned RF is competitive with
+# this. TODO: Factor in new "title" feature.
+# def predictManual(record):
+#     if record.Sex == 0:
+#         return 0
+#     else: # Females
+#         if record.Pclass == 0:
+#             return 1
+#         elif record.Pclass == 0.5:
+#             return 1
+#         else:
+#             if record.Fare > 0.05:
+#                 return 0
+#             else:
+#                 if record.Embarked == 0:
+#                     return 0
+#                 else:
+#                     return 1
 
 # Put together a list of predictions 
-pred = [predictManual(r) for r in xTest.itertuples()]
-
-# pred = clf.predict(featuresTest)
-
-
+# pred = [predictManual(r) for r in xTest.itertuples()]
+ 
+from sklearn.metrics import classification_report  
+print classification_report(labelsTest,pred)
+exit(0)
+ 
 # SUBMISSIONS
-# clf.fit(xTrain.drop('PassengerId',axis=1),yTrain)
-# pred = clf.predict(xTest.drop('PassengerId',axis=1))
+# gs.fit(xTrain.drop('PassengerId',axis=1),yTrain)
+# pred = gs.predict(xTest.drop('PassengerId',axis=1))
 
 # Create and write the submission to disk
-submission = pd.DataFrame({'Survived': pred}, index=xTest['PassengerId'])
-submission.to_csv("submission.csv")
+# submission = pd.DataFrame({'Survived': pred}, index=xTest['PassengerId'])
+# submission.to_csv("submission.csv")
 
 '''
 # Scoring for training before submission:
